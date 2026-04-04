66549f7d28438f1bcc65787e79c8d8f99f23373c
Shaked Amihai <shakedamihai@gmail.com>
2026-04-04 11:25:41 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { SpaceProvider } from "./contexts/SpaceContext"; // הנתיב לקובץ הקונטקסט החדש
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <SpaceProvider>
+    <App />
+  </SpaceProvider>
+);
